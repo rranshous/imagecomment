61957f99b462ca4c453f933dc97efe1bdfc6d31f
robby@hobo <none@none>
2010-11-11 12:40:42 -0500

    updated look, working on user editing

--- controllers/users.py
+++ controllers/users.py
@@ -9,8 +9,24 @@ class User:
         return 'create'
 
     @cherrypy.expose
-    def update(self,**kwargs):
-        return 'update'
+    def update(self,handle=None,email=None,password1=None,
+                    password2=None,action=None):
+        """ users can only edit themselves """
+        user = cherrypy.request.user
+        try:
+            if action:
+                if handle:
+                    user.handle = handle
+                if email:
+                    user.email_address = email
+                if password1:
+                    if password1 != password2:
+                        raise e.ValidationException('Passwords do not match')
+                    user.password = password1
+                redirect('/user/%s' % user.id)
+        except Exception, ex:
+            raise
+            add_flash('error','%s' % ex)
 
     @cherrypy.expose
     def delete(self,**kwargs):
