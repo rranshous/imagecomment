6fa358439d4ee2733ac678b4ce6b87725ec345d1
Robby Ranshous <rranshous@gmail.com>
2011-07-26 16:51:26 -0400

    added login page sets

--- auth.py
+++ auth.py
@@ -27,25 +27,43 @@ def get_user_from_session():
 def get_user():
     return get_user_from_session()
 
-def check_active_login():
+def check_active_login(skip=False,login=True):
     """
     checks that there is an active user or sends back a 403
     """
 
-    # make sure there is a handle
-    if not cherrypy.session.get('user_handle'):
-        error(403)
+    try:
+        cherrypy.log('check active login skip: %s' % skip)
+        if skip:
+            cherrypy.log('skipping check public login')
+            return True
 
-    # make sure there's a hash in the session
-    if not cherrypy.session.get('user_hash'):
-        error(403)
+        cherrypy.log('checking active login')
 
-    # find the user and check the hash against his password
-    user = m.User.get_by(handle=cherrypy.session.get('user_handle'))
-    if not user:
-        error(403)
-    if hash_password(user.password) != cherrypy.session.get('user_hash'):
-        error(403)
+        # make sure there is a handle
+        if not cherrypy.session.get('user_handle'):
+            cherrypy.log('no user handle found')
+            error(403)
+
+        # make sure there's a hash in the session
+        if not cherrypy.session.get('user_hash'):
+            cherrypy.log('no user hash found')
+            error(403)
+
+        # find the user and check the hash against his password
+        user = m.User.get_by(handle=cherrypy.session.get('user_handle'))
+        if not user:
+            cherrypy.log('no user by that handle found')
+            error(403)
+        if hash_password(user.password) != cherrypy.session.get('user_hash'):
+            cherrypy.log('user password hash does not match')
+            error(403)
+
+    except HTTPError:
+        if login:
+            redirect('/login')
+        else:
+            raise
 
     return user
 
@@ -56,29 +74,32 @@ def auth_credentials(handle,password):
         return False
     return user
 
-def login_user(user):
+def set_logged_in_user(user):
     """ updates the session from the user object """
+    cherrypy.log('setting user in session: %s' % user.handle)
     cherrypy.session['user_hash'] = hash_password(user.password)
     cherrypy.session['user_handle'] = user.handle
 
+def login_user(handle,password):
+    cherrypy.log('login_user: %s' % handle)
+    user = auth_credentials(handle,password)
+    if user:
+        cherrypy.log('login successful')
+        set_logged_in_user(user)
+    else:
+        cherrypy.log('login failed')
+        return False
+    return True
+
 def logout_user():
-    cherrypy.lib.sessions.expire()
+    cherrypy.log('loging out user')
+    if 'user_handle' in cherrypy.session:
+        del cherrypy.session['user_handle']
+    if 'user_hash' in cherrypy.session:
+        del cherrypy.session['user_hash']
 
-@decorator
-def public(f,*args,**kwargs):
-    f._cp_config = {'cherrypy.tools.check_active_login.on':False}
-    return f(*args,**kwargs)
+def public(f):
+    f._cp_config = {'tools.check_active_login.skip':True}
+    cherrypy.log('making public: %s' % f.__name__)
+    return f
 
-def public_redirect(new_url):
-    """ if the user isn't logged in pushes to another url """
-    def _public_redirect(f):
-        def __public_redirect(*args,**kwargs):
-            # check the login
-            try:
-                check_active_login()
-            except HTTPError:
-                redirect(new_url)
-
-            return f(*args,**kwargs)
-        return __public_redirect
-    return _public_redirect
--- controllers/root.py
+++ controllers/root.py
@@ -4,8 +4,9 @@ from tags import Tag
 from users import User
 from comments import Comment
 from albums import Album
-from helpers import render, redirect
-from auth import public, logout_user, public_redirect
+from helpers import render, redirect, add_flash
+from auth import public, logout_user, login_user
+import models as m
 
 class Root:
     """ sits @ The root of the app """
@@ -17,24 +18,55 @@ class Root:
     default = Media() # other than the above options it's media
 
     @cherrypy.expose
-    @public_redirect('/login')
     def index(self):
         """ login if not authed else the home page """
         return render('/index.html')
 
     @cherrypy.expose
     @public
-    def login(self,username=None,password=None,action=None):
+    def login(self,username=None,password=None,action=None,endpoint=None,
+                   new_user=None):
         """ prompts the user to login, creates the user if it doesn't exist """
 
         # log them out
         logout_user()
 
         # see if they are trying to login / create user
-        if action:
+        if action and username or password:
+
+            # if they didn't provide a password than we need to
+            # either create a new user or prompt them for pass if one exists
+            if not password:
+                user = m.User.get_by(handle=username)
+                if user:
+                    return render('/login_password.html',username=username)
+
+                else:
+                    return render('/login_new_user.html',username=username)
+
+            # if we are creating a new user, do so
+            if new_user == 'true':
+                user = m.User(handle=username)
+                user.password = password
+                m.session.commit()
+
             # if they gave us a username and a password then they are
             # trying to login
-            pass
+            if not login_user(username,password):
+                # fail !
+                add_flash('error','Your login has failed!')
+
+            else:
+                cherrypy.log('successfull login: %s' % username)
+
+                # yay, success. push them to their next point
+                if endpoint:
+                    redirect(endpoint)
+
+                # or home
+                else:
+                    redirect('/')
+
 
         return render('/login_front.html',username=username)
 
